34a63204f90d2bb3d654479f323cb4d3bf1bfb7f
jose8a <ochovilla@gmail.com>
2017-12-23 00:48:16 -0800

    Add parsing for WWR customer support jobs

--- app/routes/remotes-misc.js
+++ app/routes/remotes-misc.js
@@ -1,7 +1,8 @@
 // =================================================================
-// '/remote/'             -- Returns all stories available from all tech sources
+// '/remote/'             -- Returns all remote jobs available from all tech sources
 // '/remote/list'         -- Returns a list of the available tech news sources available via the API
-// '/remote/wwr'          -- Returns all the job listings from WWR Programmer category page
+// '/remote/wwr-prog'     -- Returns all the job listings from WWR 'Programmer' category page
+// '/remote/wwr-cust'     -- Returns all the job listings from WWR 'Customer Support' category page
 // '/remote/wfh'          -- Returns all the job listings from WFH
 // '/remote/sov'          -- Returns all the job listings from Stack Overflow remotes
 // '/remote/ghub'         -- Returns all the job listings from Github remote listings
@@ -9,16 +10,17 @@
 //
 // =================================================================
 let fetchWWRProgJobs = require("../parsers/remote/wwr-prog");
+let fetchWWRCustJobs = require("../parsers/remote/wwr-cust");
 // --- let fetchWFHJobs = require("../parsers/remote/wfh");
 // --- let fetchSOVJobs = require("../parsers/remote/sov-rem");
 // --- let fetchGHubJobs = require("../parsers/remote/ghub-rem");
 // --- let fetchSpressoJobs = require("../parsers/remote/spresso");
 
 module.exports = function(router) {
-  // get a collection of all my available stories from all sources
+  // get a collection of all my available remote jobs from all sources
   router.get('/', (req, res, next) => {
     console.log("ALL NON-CL REMOTE JOBS - path: '/remote/'");
-    res.status(200).json({stories: {sov: "tbd", wwr: "tbd", wfh: "tbd", ghub: "tbd", spresso: "tbd"}});
+    res.status(200).json({jobs: {sov: "tbd", wwr: "tbd", wfh: "tbd", ghub: "tbd", spresso: "tbd"}});
   });
 
   // get list of all available tech sources
@@ -27,35 +29,42 @@ module.exports = function(router) {
     res.status(200).json({sources: ['sov', 'wwr', 'wfh', 'ghub', 'spresso']});
   });
 
-  // get list of all stories available on FEFront front-page
+  // get list of all remote jobs available on Stack Overflow
   router.get('/sov', (req, res, next) => {
     console.log("Stack Overflow Jobs - path: '/remote/sov'");
     res.status(200).send('TODO: Endpoint remote/sov not yet implemented');
     // --- fetchSOVJobs(req, res);
   });
 
-  // get list of all stories available on EchoJS front-page
+  // get list of all remote programming-jobs available on WWR
   router.get('/wwr-prog', (req, res, next) => {
-    console.log("WWR Jobs- path: '/remote/wwr'");
+    console.log("WWR Jobs- path: '/remote/wwr-prog'");
     res.status(200).send('TODO: Endpoint remote/wwr-prog not yet implemented');
     // --- fetchWWRProgJobs(req, res);
   });
 
-  // get list of all stories available on CSS Tricks front-page
+  // get list of all remote customer-support-jobs available on WWR
+  router.get('/wwr-cust', (req, res, next) => {
+    console.log("WWR Jobs- path: '/remote/wwr-cust'");
+    // --- res.status(200).send('TODO: Endpoint remote/wwr-cust not yet implemented');
+    fetchWWRCustJobs(req, res);
+  });
+
+  // get list of all remote jobs available on WFH
   router.get('/wfh', (req, res, next) => {
     console.log("WFH Jobs - path: '/remote/wfh'");
     res.status(200).send('TODO: Endpoint remote/wfh not yet implemented');
     // --- fetchWFHJobs(req, res);
   });
 
-  // get list of all stories available on DevTo front-page
+  // get list of all remote jobs available on Github
   router.get('/ghub', (req, res, next) => {
     console.log("Github Remote Jobs - path: '/remote/ghub'");
     res.status(200).send('TODO: Endpoint remote/ghub not yet implemented');
     // --- fetchGHubJobs(req, res);
   });
 
-  // get list of all stories available on ScotchIO front-page
+  // get list of all remote jobs available on Jobspresso
   router.get('/spresso', (req, res, next) => {
     console.log("Jobspresso Jobs - path: '/remote/spresso'");
     res.status(200).send('TODO: Endpoint remote/spresso not yet implemented');
