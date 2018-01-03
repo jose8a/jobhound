44c028254ebdbb0e1af9fea9f8095e9057db8bab
jose8a <ochovilla@gmail.com>
2018-01-03 05:18:34 -0800

    Add basic testing for WFH Non-Software listings.

--- test/remotes-misc/test-wfh.js
+++ test/remotes-misc/test-wfh.js
@@ -5,11 +5,23 @@ function testWFH(api) {
   describe('WFH Remote listings', function(done) {
     let baseRoute = '/remote'
     let wfhSofListings = baseRoute + '/wfh-sof';
-    // --- let wfhAllListings = baseRoute + '/wfh-all';
+    let wfhAllListings = baseRoute + '/wfh-all';
 
     it('should get Software listings', function(done) {
       this.timeout(5000);
       api.get(wfhSofListings)
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end( function(err, res) {
+          expect(res.status).to.equal(200);
+          expect(res.body.length).to.be.at.least(1);
+          done();
+        });
+    });
+
+    it('should get Non-Software listings', function(done) {
+      this.timeout(5000);
+      api.get(wfhAllListings)
         .set('Accept', 'application/json')
         .expect(200)
         .end( function(err, res) {
