b11dd8332e94644e87b27b66a16032c6fac235b7
jose8a <ochovilla@gmail.com>
2018-01-05 01:59:25 -0800

    Change test-case description.

--- test/remotes-misc/test-jsp.js
+++ test/remotes-misc/test-jsp.js
@@ -2,11 +2,11 @@ let should = require('chai').should();
 let expect = require('chai').expect;
 
 function testJSP(api) {
-  describe('JSP Remote listings', function(done) {
+  describe('JSP listings', function(done) {
     let baseRoute = '/remote'
     let jspListings = baseRoute + '/jsp';
 
-    it('should get Programming listings', function(done) {
+    it('should get Remote listings', function(done) {
       this.timeout(5000);
       api.get(jspListings)
         .set('Accept', 'application/json')
